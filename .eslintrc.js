412a38ff1a270a8733529a22a4270d50248a0205
Cesar <camongez28@gmail.com>
2022-09-25 21:44:42 -0300

    git init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'standard',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    printWidth: 120,
+    singleQuote: true,
+    trailingComma: 'es5',
+  },
+};
