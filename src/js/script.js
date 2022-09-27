838db831f2b395934cc71f09a4c1329be7b54719
Cesar <camongez28@gmail.com>
2022-09-27 10:12:05 -0300

    All project logic implemented.

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,3 @@
-import { getAllProducts, getAllCategories } from './services.js';
-
 const URL = 'https://bsale-backend-cesar-mongez.herokuapp.com';
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -9,46 +7,102 @@ document.addEventListener('DOMContentLoaded', async () => {
   await renderCategories(categories);
 });
 
-let inputSearch = document.getElementById('inputSearch');
-inputSearch.addEventListener('keyup', (event) => {
-  searchProductos(event.target.value);
-});
+//Get data from api
 
+const getAllProducts = async () => {
+  try {
+    const request = await fetch(`${URL}/v1/products/`);
+    const response = await request.json();
+    return response;
+  } catch (error) {
+    console.log('Error =>:', error);
+  }
+};
+
+const getAllProductsNav = async () => {
+  try {
+    const products = await getAllProducts();
+    await renderCards(products);
+  } catch (error) {
+    console.log('Error: ', error);
+  }
+};
+
+const getAllCategories = async () => {
+  try {
+    const request = await fetch(`${URL}/v1/categories/`);
+    const response = await request.json();
+    return response;
+  } catch (error) {
+    console.log('Error =>:', error);
+  }
+};
+
+const getCategory = async (id) => {
+  try {
+    const request = await fetch(`${URL}/v1/products/category/${id}`);
+    const response = await request.json();
+    await console.log(response);
+    await renderCards(response);
+  } catch (error) {
+    console.log('Error =>:', error);
+  }
+};
+
+//Renders
+//Render cards
 const renderCards = async (productsData) => {
   let containerCards = document.querySelector('#productsContainer');
   containerCards.innerHTML = '';
   productsData.forEach((product) => {
-    containerCards.innerHTML += `<div class="col-sm-5 col-md-3 col-lg-2 mb-4 card">
-  <img src="${
-    product.url_image == '' || product.url_image == null ? `./src/img/img-not-available.png` : product.url_image
-  }" 
-      class="card-img-top" alt="...">
-      <div class="card-body">
-      <h5 class="card-title">${product.name}</h5>
-      <p class="card-text">$ ${product.price}</p>
-      <a href="#" class="btn btn-primary">Comprar</a>
-  </div>
+    containerCards.innerHTML += `
+  <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+    <div class="card h-100">
+        <div class="card-img-container"> <img src="${
+          product.url_image == '' || product.url_image == null ? `./src/img/img-not-available.png` : product.url_image
+        }" class="card-img-top" alt="..."></div>
+        <div class="card-body d-flex flex-column justify-content-end">
+            <h5 class="card-title">${product.name}</h5>
+            ${
+              product.discount > 0
+                ? `
+            <div class="card-price">
+                <span>$${((100 - product.discount) * product.price) / 100}
+                </span>
+                <span>$${product.price}</span>
+            </div>`
+                : `<div>$${product.price}</div>`
+            }
+            <a href="#" class="btn btn-bsale text-white">Comprar</a>
+        </div>
+    </div>
 </div>`;
   });
 };
+
+//Render navbar
 const renderCategories = async (categories) => {
-  console.log(categories);
   let containerNav = document.getElementById('navCategories');
   containerNav.innerHTML = `
   <li class="nav-item">
-  <a class="nav-link" href="#">
+  <a class="nav-link" onclick="getAllProductsNav()" href="#">
     Todos los productos
   </a>
   </li>`;
-
   categories.forEach((category) => {
-    containerNav.innerHTML += ` 
+    containerNav.innerHTML += `
 
   <li class="nav-item">
-  <a class="nav-link" id="${category.id}" href="#">${category.name}</a>
+  <a class="nav-link" onclick="getCategory(${category.id})" id="${category.id}" href="#">${category.name}</a>
   </li>`;
   });
 };
+
+//Search realtime
+let inputSearch = document.getElementById('inputSearch');
+inputSearch.addEventListener('keyup', (event) => {
+  searchProductos(event.target.value);
+});
 
 const searchProductos = async (inputValue) => {
   try {
@@ -56,16 +110,6 @@ const searchProductos = async (inputValue) => {
     const response = await request.json();
     await renderCards(response);
     return response;
-  } catch (error) {
-    console.log('Error =>:', error);
-  }
-};
-
-const getCategory = async (category = '') => {
-  try {
-    const request = await fetch(`${URL}/v1/categories/${category}`);
-    const response = await request.json();
-    await renderCards(response);
   } catch (error) {
     console.log('Error =>:', error);
   }
